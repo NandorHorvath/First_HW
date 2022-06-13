90a8f238fd24a58eb97e529921bb4f1d956e41dc
NandorHorvath <71963377+NandorHorvath@users.noreply.github.com>
2022-06-13 18:46:00 +0200

    Add files via upload

---
+++ hiba.js
@@ -0,0 +1,9 @@
+function showError(){
+
+    var x = document.getElementById("error");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
